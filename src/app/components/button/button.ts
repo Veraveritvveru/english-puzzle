b360857ce3f154d2ccb00a84b702c7066146779c
Vera Filimonova <filimonovavera96@gmail.com>
2025-03-24 23:28:14 +0300

    start validation

---
+++ src/app/components/button/button.ts
@@ -0,0 +1,16 @@
+import './button.scss';
+
+import BaseComponent from "../base-component";
+
+type typeBtn = 'submit';
+
+export class Button extends BaseComponent {
+   constructor(typeName: typeBtn, textContent?: string) {
+      super({
+         tagName: 'button',
+         classNames: ['button'],
+         attributes: { type: typeName },
+         textContent: textContent,
+      })
+   }
+}
--- src/app/components/form/form.ts
+++ src/app/components/form/form.ts
@@ -1,4 +1,7 @@
+import './form.scss';
+
 import BaseComponent from "../base-component";
+import { Button } from '../button/button';
 
 const INPUT_FIELDS_ARR = ['First Name', 'Surname'];
 const ID_ARR = ['firstName', 'surname'];
@@ -7,12 +10,23 @@ export class FormComponent extends BaseComponent {
   constructor() {
     super({ tagName: 'form', classNames: ['login__form'] });
 
+    this.drawTitle();
     this.drawInputs();
+    this.drawSubmitButton();
+  }
+
+  drawTitle() {
+    const loginTitle = new BaseComponent({
+      tagName: 'h2', 
+      classNames:['login__title'],
+      textContent: 'Login',
+    })
+    this.append(loginTitle);
   }
 
   drawInputs() {
     const inputs = INPUT_FIELDS_ARR.map((elem, i) => {
-      const div = new BaseComponent({tagName: 'div', classNames: [`input__wrapper${i}`]});
+      const div = new BaseComponent({tagName: 'div', classNames: ['input__wrapper']});
       const label = new BaseComponent({
         tagName: 'label',
         classNames: ['input-label'],
@@ -23,11 +37,16 @@ export class FormComponent extends BaseComponent {
         tagName: 'input',
         classNames: ['input'],
         attributes: {id: ID_ARR[i]}
-       });
+      });
     div.append(label, input);
     return div;
     })
 
     this.append(...inputs)
+  }
+
+  drawSubmitButton() {
+    const submitBtn = new Button('submit', 'Log in');
+    this.append(submitBtn);
   }
 }
